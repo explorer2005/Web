edf21cfb32d6fba717d2df9e2d6898450e3dbd93
Dhruv <dhruvguptaofficial2005@gmail.com>
2025-02-26 11:44:23 +0530

    basics of backened

---
+++ Lecture55/practice.js
@@ -0,0 +1,16 @@
+let d="DHRUV"
+console.log(d)
+console.log(typeof d)
+d=d+10
+console.log(d)
+console.log(typeof d)
+
+const obj={
+    dh:100,
+}
+console.log(obj)
+obj.dh=1000
+console.log(obj)
+
+obj.no=true
+console.log(obj)
---
+++ Lecture55/script.js
@@ -0,0 +1,40 @@
+console.log("Hello you logged into the console")
+var a=5;
+var b=6;
+console.log(a+b)
+var c="DHRUV"
+console.log(typeof a,typeof b,typeof c)
+const a1=6
+console.log(a1)
+
+
+let d=10
+console.log(d);
+d=d+10
+console.log(d)
+{
+    let d=100;
+    console.log(d);
+}
+console.log(d)
+
+let x="DHRUV"
+let y=22
+let z=3.55
+const p=true;
+let q=undefined
+let r=null
+
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+let item={
+    name:"DHRUV",
+    age:19,
+    gender:"Male",
+    "Full Address":"Mathikere extension"
+}
+console.log(item)
+item.relation="DADDY"
+console.log(item)
+
